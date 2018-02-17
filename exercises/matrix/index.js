18ec816f0c6fbfdc2850e5c75a6a48f3ccc645bd
Samatar.Axmed <samatar.axmed@livenation.co.uk>
2018-02-17 12:57:58 +0000

    Add eslint

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -14,7 +14,6 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
-
 function matrix(n) {}
 
-module.exports = matrix;
+module.exports = matrix
---
+++ exercises/matrix/solution.md
@@ -0,0 +1,1 @@
+![solution-description](https://user-images.githubusercontent.com/22747985/36064409-86eeb244-0e82-11e8-8ac2-69bc630dd6de.png)
